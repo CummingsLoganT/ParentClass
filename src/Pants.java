0caf2facb79e23b94cdc6ccb7d69dadf5ed49ceb
CummingsLoganT <109982415+CummingsLoganT@users.noreply.github.com>
2022-09-23 17:14:21 -0500

    initial commit

---
+++ src/Pants.java
@@ -0,0 +1,8 @@
+public class Pants {
+
+    private String size;
+    private double price;
+    private String color;
+    private String brand;
+
+}
---
+++ src/Shirt.java
@@ -0,0 +1,8 @@
+public class Shirt {
+
+    private String size;
+    private double price;
+    private String color;
+    private String brand;
+
+}
